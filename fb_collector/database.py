b1529d23c597584896f2418a6dacf5ddc0e87113
cintageorge <cintageorge212@gmail.com>
2017-02-11 13:44:39 +0530

    Update database.py

--- fb_collector/database.py
+++ fb_collector/database.py
@@ -3,7 +3,7 @@ import pymysql
 
 class database:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,birthday):
         self.username = username
         self.password = password
         db = pymysql.connect(host='localhost', user=username, password=password, use_unicode=True, charset="utf8")
@@ -15,51 +15,51 @@ class database:
         sql = 'USE FB_DATA'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS USER(ID NUMERIC(70) PRIMARY KEY,' \
+        sql = 'CREATE TABLE IF NOT EXISTS USER(ID NUMERIC(50) PRIMARY KEY,' \
               'NAME CHAR(25) NOT NULL,' \
               'LOCATION CHAR(40),' \
               'GENDER CHAR(10) NOT NULL,' \
-              'BIRTHDAY DATE)'
+              'BIRTHDAY NUMERIC(5))'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS MOVIES(MID NUMERIC(100) PRIMARY KEY,' \
+        sql = 'CREATE TABLE IF NOT EXISTS MOVIES(MID NUMERIC(50) PRIMARY KEY,' \
               'MNAME CHAR(25) NOT NULL)'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS MUSIC(SID NUMERIC(100) PRIMARY KEY,' \
+        sql = 'CREATE TABLE IF NOT EXISTS MUSIC(SID NUMERIC(50) PRIMARY KEY,' \
               'SNAME CHAR(25) NOT NULL)'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS LIKES(LID NUMERIC(100) PRIMARY KEY,' \
+        sql = 'CREATE TABLE IF NOT EXISTS LIKES(LID NUMERIC(50) PRIMARY KEY,' \
               'LNAME CHAR(25) NOT NULL)'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS INTERESTEDMOVIES(ID NUMERIC(70),' \
-              'MID NUMERIC(100),' \
+        sql = 'CREATE TABLE IF NOT EXISTS INTERESTEDMOVIES(ID NUMERIC(50),' \
+              'MID NUMERIC(50),' \
               'FOREIGN KEY(MID) REFERENCES MOVIES(MID),' \
               'PRIMARY KEY(ID,MID))'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS INTERESTEDMUSIC(ID NUMERIC(70),' \
-              'SID NUMERIC(100),' \
+        sql = 'CREATE TABLE IF NOT EXISTS INTERESTEDMUSIC(ID NUMERIC(50),' \
+              'SID NUMERIC(50),' \
               'FOREIGN KEY(SID) REFERENCES MUSIC(SID),' \
               'PRIMARY KEY(ID,SID))'
         cur.execute(sql)
 
-        sql = 'CREATE TABLE IF NOT EXISTS LIKEDPAGES(ID NUMERIC(70),' \
-              'LID NUMERIC(100),' \
+        sql = 'CREATE TABLE IF NOT EXISTS LIKEDPAGES(ID NUMERIC(50),' \
+              'LID NUMERIC(50),' \
               'FOREIGN KEY(LID) REFERENCES LIKES(LID),' \
               'PRIMARY KEY(ID,LID))'
 
         cur.execute(sql)
 
-    def Insert_User(self,id, name, location, gender):
+    def Insert_User(self,id, name, location, gender,birthday):
         db = pymysql.connect(host='localhost', user=self.username, password=self.password)
         cur = db.cursor()
         sql = 'USE FB_DATA'
         cur.execute(sql)
-        sql = "INSERT INTO USER(ID,NAME,LOCATION,GENDER) VALUES(" + str(
-            id) + ",'" + name + "','" + location + "','" + gender + "')"
+        sql = "INSERT INTO USER(ID,NAME,LOCATION,GENDER,BIRTHDAY) VALUES(" + str(
+            id) + ",'" + name + "','" + location + "','" + gender + "','" + str(birthday) + "')"
         cur.execute(sql)
         db.commit()
 
